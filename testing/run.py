855d858979abd510cd8fc18838a6bac4d226d8aa
Patrick Deubel <patrick.deubel@gmail.com>
2020-05-06 20:40:39 +0200

    Other run config

--- testing/run.py
+++ testing/run.py
@@ -1,10 +1,10 @@
 import os
 
-os.system('mpirun --use-hwthread-cpus -np 40 python3 -m mpi-algorithms --env="Hopper-v2" --seed=0 --num-timesteps=15000 --algorithm="TRPO"')
-os.system('mpirun --use-hwthread-cpus -np 40 python3 -m mpi-algorithms --env="Hopper-v2" --seed=0 --num-timesteps=15000 --algorithm="PPO1"')
+os.system('mpirun --use-hwthread-cpus -np 40 python3 -m mpi-algorithms --env="Hopper-v2" --seed=0 --num-timesteps=1e9 --algorithm="TRPO"')
+#os.system('mpirun --use-hwthread-cpus -np 40 python3 -m mpi-algorithms --env="Hopper-v2" --seed=0 --num-timesteps=15000 --algorithm="PPO1"')
 
-os.system('mpirun --use-hwthread-cpus -np 40 python3 -m mpi-algorithms --env="HalfCheetah-v2" --seed=0 --num-timesteps=15000 --algorithm="TRPO"')
-os.system('mpirun --use-hwthread-cpus -np 40 python3 -m mpi-algorithms --env="HalfCheetah-v2" --seed=0 --num-timesteps=15000 --algorithm="PPO1"')
+#os.system('mpirun --use-hwthread-cpus -np 40 python3 -m mpi-algorithms --env="HalfCheetah-v2" --seed=0 --num-timesteps=15000 --algorithm="TRPO"')
+#os.system('mpirun --use-hwthread-cpus -np 40 python3 -m mpi-algorithms --env="HalfCheetah-v2" --seed=0 --num-timesteps=15000 --algorithm="PPO1"')
 
-os.system('mpirun --use-hwthread-cpus -np 40 python3 -m mpi-algorithms --env="Walker2d-v2" --seed=0 --num-timesteps=15000 --algorithm="TRPO"')
-os.system('mpirun --use-hwthread-cpus -np 40 python3 -m mpi-algorithms --env="Walker2d-v2" --seed=0 --num-timesteps=15000 --algorithm="PPO1"')
+#os.system('mpirun --use-hwthread-cpus -np 40 python3 -m mpi-algorithms --env="Walker2d-v2" --seed=0 --num-timesteps=15000 --algorithm="TRPO"')
+#os.system('mpirun --use-hwthread-cpus -np 40 python3 -m mpi-algorithms --env="Walker2d-v2" --seed=0 --num-timesteps=15000 --algorithm="PPO1"')
