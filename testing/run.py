743c83f084e624b7c19877db53ad65f943bb8967
Patrick Deubel <patrick.deubel@gmail.com>
2020-05-06 19:59:17 +0200

    Added test runs for the three MuJoCo environments.

--- testing/run.py
+++ testing/run.py
@@ -1,3 +1,10 @@
 import os
 
-os.system('mpirun --use-hwthread-cpus -np 40 python3 -m mpi-algorithms --env="CartPole-v1" --seed=0 --num-timesteps=100000 --algorithm="TRPO"')
+os.system('mpirun --use-hwthread-cpus -np 40 python3 -m mpi-algorithms --env="Hopper-v2" --seed=0 --num-timesteps=15000 --algorithm="TRPO"')
+os.system('mpirun --use-hwthread-cpus -np 40 python3 -m mpi-algorithms --env="Hopper-v2" --seed=0 --num-timesteps=15000 --algorithm="PPO1"')
+
+os.system('mpirun --use-hwthread-cpus -np 40 python3 -m mpi-algorithms --env="HalfCheetah-v2" --seed=0 --num-timesteps=15000 --algorithm="TRPO"')
+os.system('mpirun --use-hwthread-cpus -np 40 python3 -m mpi-algorithms --env="HalfCheetah-v2" --seed=0 --num-timesteps=15000 --algorithm="PPO1"')
+
+os.system('mpirun --use-hwthread-cpus -np 40 python3 -m mpi-algorithms --env="Walker2d-v2" --seed=0 --num-timesteps=15000 --algorithm="TRPO"')
+os.system('mpirun --use-hwthread-cpus -np 40 python3 -m mpi-algorithms --env="Walker2d-v2" --seed=0 --num-timesteps=15000 --algorithm="PPO1"')
