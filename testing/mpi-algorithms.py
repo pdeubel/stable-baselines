9df084110b1ba9f35ee8bb6c31eeb5053a00393f
Patrick Deubel <patrick.deubel@gmail.com>
2020-05-13 11:48:15 +0200

    Fixed issue where save_dir was different across MPI workers because of a second. Removed tensorboard log.

--- testing/mpi-algorithms.py
+++ testing/mpi-algorithms.py
@@ -11,24 +11,19 @@ import os
 import time
 
 
-def train(env_id, num_timesteps, seed, algorithm, model_save_file=None, tensorboard_log=None):
+def train(env_id, num_timesteps, seed, algorithm, model_save_file=None, log_dir=None):
 
     with tf_util.single_threaded_session():
-        rank = MPI.COMM_WORLD.Get_rank()
-        if rank == 0:
-            logger.configure(folder=os.path.join(tensorboard_log, "legacy"))
-        else:
-            logger.configure(folder=os.path.join(tensorboard_log, "legacy"))
-            logger.set_level(logger.DEBUG)
+        logger.configure(folder=log_dir, format_strs=['stdout', 'log', 'csv'])
 
         workerseed = seed + MPI.COMM_WORLD.Get_rank()
         env = make_mujoco_env(env_id, workerseed)
 
         if algorithm == "TRPO":
-            model = TRPO(MlpPolicy, env, seed=workerseed, verbose=1, tensorboard_log=tensorboard_log)
+            model = TRPO(MlpPolicy, env, seed=workerseed, verbose=1)
         else:
             # Algorithm is PPO
-            model = PPO1(MlpPolicy, env, seed=workerseed, verbose=1, tensorboard_log=tensorboard_log)
+            model = PPO1(MlpPolicy, env, seed=workerseed, verbose=1)
 
         model.learn(total_timesteps=num_timesteps)
 
@@ -46,17 +41,27 @@ def main():
     except FileExistsError:
         pass
 
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    if rank == 0:
+        save_dir = [os.path.join(main_dir, time.strftime('%Y_%m_%d-%Hh_%Mm_%Ss', time.localtime(time.time())))]
+    else:
+        save_dir = None
+
+    save_dir = comm.bcast(save_dir, root=0)
+
+    # Unpack list
+    save_dir = save_dir[0]
+
     parser = mujoco_arg_parser()
     parser.add_argument('--algorithm', help="The algorithm which shall be used, TRPO or PPO", type=str, default="TRPO")
     args = parser.parse_args()
-
-    save_dir = os.path.join(main_dir, time.strftime('%Y_%m_%d-%Hh_%Mm_%Ss', time.localtime(time.time())))
 
     model_file = os.path.join(save_dir, "model")
     log_dir = os.path.join(save_dir, "log")
 
     train(args.env, num_timesteps=args.num_timesteps, seed=args.seed, algorithm=args.algorithm,
-          model_save_file=model_file, tensorboard_log=log_dir)
+          model_save_file=model_file, log_dir=log_dir)
 
 
 if __name__ == '__main__':
